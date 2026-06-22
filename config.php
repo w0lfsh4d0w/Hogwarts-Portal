c442318e7be15040b821274991712a4de6638f85
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-06-22 22:01:46 +0200

    I the project in docker container

--- config.php
+++ config.php
@@ -2,9 +2,11 @@
 
 return [
     'database' => [
-        'host' => 'localhost',
-        'port' => 3306,
-        'dbname' => 'hogwarts_db',
-        'charset' => 'utf8mb4'
+        'host' => getenv('DB_HOST') ?: 'localhost',
+        'port' => getenv('DB_PORT') ?: 3306,
+        'dbname' => getenv('DB_DATABASE') ?: 'hogwarts_db',
+        'charset' => 'utf8mb4',
+        'username' => getenv('DB_USERNAME') ?: 'root',
+        'password' => getenv('DB_PASSWORD') ?: 'MyRoot@1234',
     ]
 ];
